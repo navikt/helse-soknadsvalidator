d4ee94be2efc191c5ee6047dbc2c7ca5c760d582
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-12-18 12:23:53 +0100

    Bumped streams and cleaned up build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,9 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlin_version: String by extra
-
 val junitJupiterVersion = "5.3.1"
 val spekVersion = "1.2.1"
 val kluentVersion = "1.41"
-val slf4jVersion = "1.7.25"
-val ktorVersion = "1.0.0"
-val prometheusVersion = "0.5.0"
-val kafkaVersion = "2.0.0"
-val confluentVersion = "5.0.0"
-val orgJsonVersion = "20180813"
-
 
 val mainClass = "no.nav.helse.AppKt"
 
@@ -20,13 +11,11 @@ plugins {
     application
     kotlin("jvm") version "1.3.11"
     id("com.github.johnrengelman.shadow") version "4.0.3"
-
 }
 
 buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.0")
-
     }
 }
 
@@ -36,19 +25,7 @@ application {
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    compile("org.slf4j:slf4j-simple:$slf4jVersion")
-    compile("io.ktor:ktor-server-netty:$ktorVersion")
-    compile("no.nav.helse:streams:10")
-
-
-    implementation("io.prometheus:simpleclient_common:$prometheusVersion")
-    implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
-
-    compile("org.apache.kafka:kafka-clients:$kafkaVersion")
-    compile("org.apache.kafka:kafka-streams:$kafkaVersion")
-    compile("io.confluent:kafka-streams-avro-serde:$confluentVersion")
-
-    api("org.json:json:$orgJsonVersion")
+    compile("no.nav.helse:streams:16")
 
     testCompile("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
@@ -65,15 +42,11 @@ dependencies {
         exclude(group = "org.junit.platform")
         exclude(group = "org.jetbrains.kotlin")
     }
-
-    testImplementation("no.nav:kafka-embedded-env:2.0.1")
 }
 
 repositories {
     maven("https://dl.bintray.com/kotlin/ktor")
     maven("http://packages.confluent.io/maven/")
-    maven("https://kotlin.bintray.com/kotlinx")
-    //maven("https://bintray.com/navikt/maven")
 
     jcenter()
     mavenCentral()
